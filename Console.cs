6ab8639fdfc54810dbaff91ecf3038866f742a88
GruntTheDivine <sloancrandell@gmail.com>
2013-02-02 14:39:17 -0500

    test

--- Console.cs
+++ Console.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+// Grunt was here!
 namespace dewitcher
 {
     /// <summary>
---
+++ commit.sh
@@ -0,0 +1,15 @@
+git init
+ 
+# Add the new files
+git add .
+ 
+# Clean
+git gc
+ 
+# Commit
+echo "Type a message for this commit:"
+read msg
+git commit -m $msg
+ 
+# Push
+git push -u dewitcher master
