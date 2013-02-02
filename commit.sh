528d5ee54047dc48c2c7a3a26dc46a16abc7a5ff
SplitOS <dasmarcooo@gmail.com>
2013-02-02 21:55:33 +0100

    Test of the new commit script

--- commit.sh
+++ commit.sh
@@ -1,15 +1,42 @@
+clear
+#############################################
+#            dewitcher Framework            #
+#               Commit Script               #
+#############################################
+#      Copyright (C) GruntXProductions      #
+#############################################
+echo.
+
+echo "#############################################"
+echo "#          Initializing Repository          #"
+echo "#############################################"
 git init
- 
-# Add the new files
+echo.
+
+clear
+echo "#############################################"
+echo "#            Merging contents...            #"
+echo "#############################################"
+git pull
+echo.
+
+clear
+echo "#############################################"
+echo "#             Adding content...             #"
+echo "#############################################"
 git add .
- 
-# Clean
-git gc
- 
-# Commit
-echo "Type a message for this commit:"
+echo.
+
+clear
+echo "#############################################"
+echo "#   Please type a message for this commit   #"
+echo "#############################################"
 read msg
 git commit -m "$msg"
- 
-# Push
+echo.
+
+clear
+echo "#############################################"
+echo "#            Uploading content..            #"
+echo "#############################################"
 git push -u dewitcher master
