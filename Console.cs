68df146eca8d0e95790dbab03e29fcf3ea6ab8ce
GruntTheDivine <sloancrandell@gmail.com>
2013-02-02 15:38:22 -0500

    Added PushContents and PopContents to Console

--- Console.cs
+++ Console.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 // Grunt was here!
 namespace dewitcher
 {
+    
     /// <summary>
     /// A Console class that contains so much cool features for OS developing ;)
     /// </summary>
-    public static partial class Console
+    public unsafe static partial class Console
     {
+        class VideoBuffer
+        {
+            public byte[] data;
+            public int X, Y;
+        }
+        private static Stack<VideoBuffer> videoBuffers = new Stack<VideoBuffer>();
+        /// <summary>
+        /// Pushes what is in video RAM onto a stack
+        /// </summary>
+        public static void PushContents()
+        {
+            VideoBuffer vb = new VideoBuffer();
+            byte* VideoRam = (byte*)0xB8000;
+            vb.data = new byte[4250];
+            for (int i = 0; i < 4250; i++)
+            {
+                byte b = VideoRam[i];
+                vb.data[i] = b;
+            }
+            vb.X = CursorLeft;
+            vb.Y = CursorTop;
+            videoBuffers.Push(vb);
+        }
+        public static void PopContents()
+        {
+            VideoBuffer vb = videoBuffers.Pop();
+            byte* VideoRam = (byte*)0xB8000;
+  
+            for (int i = 0; i < 4250; i++)
+            {
+                VideoRam[i] = vb.data[i];
+            
+            }
+            CursorLeft = vb.X;
+            CursorTop = vb.Y;
+        }
         private static int indent = 0;
         /// <summary>
         /// ForegroundColor Property
