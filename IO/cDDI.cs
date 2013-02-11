b753ec59df5f09ce805f591073b1b4b67d4f7355
SplitOS <dasmarcooo@gmail.com>
2013-02-11 17:13:50 +0100

    inb, inw, inl, outb, outw and outl support (IO/cDDI.cs)

---
+++ IO/cDDI.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Cosmos.Core;
+
+namespace dewitcher.IO
+{
+    public static class cDDI
+    {
+        /// <summary>
+        /// Reads a byte
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static byte inb(ushort port)
+        {
+            IOPortRead io = new IOPortRead(port);
+            return io.Byte;
+        }
+        /// <summary>
+        /// Reads a 16 bit word
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static ushort inw(ushort port)
+        {
+            IOPortRead io = new IOPortRead(port);
+            return io.Word;
+        }
+        /// <summary>
+        /// Reads a 32 bit word
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static uint inl(ushort port)
+        {
+            IOPortRead io = new IOPortRead(port);
+            return io.DWord;
+        }
+        /// <summary>
+        /// Writes a byte
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="data"></param>
+        public static void outb(ushort port, byte data)
+        {
+            IOPortWrite io = new IOPortWrite(port);
+            io.Byte = data;
+        }
+        /// <summary>
+        /// Writes a 16 bit word
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="data"></param>
+        public static void outw(ushort port, ushort data)
+        {
+            IOPortWrite io = new IOPortWrite(port);
+            io.Word = data;
+        }
+        /// <summary>
+        /// Writes a 32 bit word
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="data"></param>
+        public static void outl(ushort port, uint data)
+        {
+            IOPortWrite io = new IOPortWrite(port);
+            io.DWord = data;
+        }
+    }
+}
