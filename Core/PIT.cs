0a0d22a45288dfe5e27c89ca397acbac88f68d21
SplitOS <dasmarcooo@gmail.com>
2013-02-11 19:42:56 +0100

    Added Gruntys IDT, PIT and IRQ class

---
+++ Core/PIT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+// PIT code by Grunt
+namespace dewitcher.Core
+{
+    public static class PIT
+    {
+        public static void Init(uint frequency)
+        {
+            uint divisor = 1193180 / frequency;
+            IO.cDDI.outb(0x43, 0x36);
+            byte l = (byte)(divisor & 0xFF);
+            byte h = (byte)((divisor >> 8) & 0xFF);
+            IO.cDDI.outb(0x40, l);
+            IO.cDDI.outb(0x40, h);
+        }
+    }
+}
