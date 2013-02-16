c2606c2935b7bb49b1b254aeec5a97541f87f4e3
SplittyDev <dasmarcooo@gmail.com>
2013-02-16 05:07:02 +0100

    PCSpeaker driver, PIT Beep, Musical note frequencies

--- Core/PIT.cs
+++ Core/PIT.cs
@@ -8,7 +8,7 @@ namespace dewitcher.Core
             dewitcher.Core.IDT.Remap();
             dewitcher.Core.IRQ.ClearMask(0);
             dewitcher.Core.IRQ.ClearMask(15);
-            uint divisor = 1193182 / frequency;
+            uint divisor = 1193180 / frequency;
             IO.CDDI.outb(0x43, 0x30);
             IO.CDDI.outb(0x40, (byte)(divisor & 0xFF));
             IO.CDDI.outb(0x40, (byte)((divisor >> 8) & 0xFF));
@@ -18,10 +18,17 @@ namespace dewitcher.Core
             dewitcher.Core.IDT.Remap();
             dewitcher.Core.IRQ.ClearMask(0);
             dewitcher.Core.IRQ.ClearMask(15);
-            uint divisor = 1193182 / frequency;
+            uint divisor = 1193180 / frequency;
             IO.CDDI.outb(0x43, 0x36);
             IO.CDDI.outb(0x40, (byte)(divisor & 0xFF));
             IO.CDDI.outb(0x40, (byte)((divisor >> 8) & 0xFF));
+        }
+        public static void Beep(uint frequency)
+        {
+            uint divisor = 1193180 / frequency;
+            IO.CDDI.outb(0x43, 0xB6);
+            IO.CDDI.outb(0x42, (byte)(divisor & 0xFF));
+            IO.CDDI.outb(0x42, (byte)((divisor >> 8) & 0xFF));
         }
     }
 }
---
+++ dev/PCSpeaker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dewitcher.IO;
+
+namespace dewitcher.dev
+{
+    public static class PCSpeaker
+    {
+        public static void sound_on()
+        {
+            CDDI.outb(0x61, (byte)(CDDI.inb(0x61) | 3));
+        }
+        public static void sound_off()
+        {
+            CDDI.outb(0x61, (byte)(CDDI.inb(0x61) & ~3));
+        }
+        public static void Beep(uint frequency)
+        {
+            Core.PIT.Beep(frequency);
+            sound_on();
+        }
+        public static void Beep(uint frequency, uint milliseconds)
+        {
+            Beep(frequency);
+            RTC.SleepMilliseconds(milliseconds);
+            sound_off();
+        }
+        struct Notes
+        {
+            public static uint C1 = 33;
+            public static uint CS1 = 35;
+            public static uint D1 = 37;
+            public static uint DS1 = 39;
+            public static uint E1 = 41;
+            public static uint F1 = 44;
+            public static uint FS1 = 46;
+            public static uint G1 = 49;
+            public static uint GS1 = 52;
+            public static uint A1 = 55; // Exactly 55.000
+            public static uint AS1 = 58;
+            public static uint B1 = 62;
+
+            public static uint C2 = 65;
+            public static uint CS2 = 69;
+            public static uint D2 = 73;
+            public static uint DS2 = 78;
+            public static uint E2 = 82;
+            public static uint F2 = 87;
+            public static uint FS2 = 92;
+            public static uint G2 = 98;
+            public static uint GS2 = 104;
+            public static uint A2 = 110; // Exactly 110.000
+            public static uint AS2 = 117;
+            public static uint B2 = 123;
+        }
+    }
+}
