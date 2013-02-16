ead1089cf32fc41ac00c80a43a9e183e462090db
GruntTheDivine <sloancrandell@gmail.com>
2013-02-16 01:14:49 -0500

    test

--- commit.sh
+++ commit.sh
@@ -1,16 +1,75 @@
-git init
- 
-# Add the new files
-git add .
- 
-# Clean
-git gc
- 
-# Commit
-echo "Type a message for this commit:"
-read msg
-git commit -m "$msg"
- 
-# Push
-git push -u dewitcher master
+clear
+echo "#############################################"
+echo "#            dewitcher Framework            #"
+echo "#               Commit Script               #"
+echo "#############################################"
+echo "#        Copyright(C) dewitcher Team        #"
+echo "#############################################"
+echo "#         Press any key to continue         #"
 read
+
+clear
+echo "#############################################"
+echo "#          Initializing Repository          #"
+echo "#############################################"
+git init
+
+clear
+echo "#############################################"
+echo "# DONE Initializing Repository              #"
+echo "#############################################"
+echo "#            Merging contents...            #"
+echo "#############################################"
+git pull
+
+clear
+echo "#############################################"
+echo "# DONE Initializing Repository              #"
+echo "# DONE Merging contents                     #"
+echo "#############################################"
+echo "#             Adding content...             #"
+echo "#############################################"
+git add .
+
+clear
+echo "#############################################"
+echo "# DONE Initializing Repository              #"
+echo "# DONE Merging contents                     #"
+echo "# DONE Adding content                       #"
+echo "#############################################"
+echo "#            Cleaning Repository            #"
+echo "#############################################"
+# nothing to do
+# please use clean.sh
+
+clear
+echo "#############################################"
+echo "# DONE Initializing Repository              #"
+echo "# DONE Merging contents                     #"
+echo "# DONE Adding content                       #"
+echo "# DONE Cleaning Repository                  #"
+echo "#############################################"
+echo "#   Please type a message for this commit   #"
+echo "#############################################"
+read msg
+git commit -a -m "$msg"
+
+clear
+echo "#############################################"
+echo "# DONE Initializing Repository              #"
+echo "# DONE Merging contents                     #"
+echo "# DONE Adding content                       #"
+echo "# DONE Cleaning Repository                  #"
+echo "# DONE Committing changes                   #"
+echo "#############################################"
+echo "#            Uploading content..            #"
+echo "#############################################"
+git push -u dewitcher master
+
+echo.
+echo "#############################################"
+echo "#                   Done!                   #"
+echo "#############################################"
+read
+
+# End of Script
