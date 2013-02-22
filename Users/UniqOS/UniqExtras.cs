742d69c1fa9ba77288c44a2c7739951e60743bee
Uniqos <joshmilesproduction@gmail.com>
2013-02-22 12:24:47 +0000

    Signed-off-by: Uniqos <joshmilesproduction@gmail.com>

---
+++ Users/UniqOS/UniqExtras.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dewitcher.Users.UniqOS
+{
+    class UniqExtras
+    {
+        //Calulcate how much remaining time there is until the process is completed.
+        public static System.Diagnostics.Stopwatch xBTimer;
+
+        //Simple to user
+        /* 
+         * Use in installations or file copying...
+         * e.g
+         * CalulcateRemaingTime(50, 100)
+         * 50 tasks completed out of 100!
+         * */
+        public TimeSpan CalculateRemainingTime(int completedCount, int totalCount)
+        {
+            if (xBTimer == null)
+            {
+                xBTimer = new System.Diagnostics.Stopwatch();
+                xBTimer.Start();
+            }
+
+            long percentComplete = (completedCount * 100 / totalCount);
+            if (percentComplete == 0)
+            { //DIVIDING BY 0 WILL MEAN DEATH. Should call the burn() method..... xD
+                percentComplete = 1;
+            }
+            long remaining = (xBTimer.ElapsedMilliseconds / percentComplete)
+                * (100 - percentComplete);
+
+            return new TimeSpan(remaining * 10000);
+        }
+    }
+}
